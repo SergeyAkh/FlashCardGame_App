da93dddc0d79a8756c2abb7caf150d34ce9ec217
Sergey <akhmadulinsergey@gmail.com>
2023-12-11 20:56:47 +0200

    New

--- app/src/main/java/com/example/test_app_2/MainActivity.java
+++ app/src/main/java/com/example/test_app_2/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.test_app_2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -53,25 +58,36 @@ public class MainActivity extends AppCompatActivity {
     private static final String KEY_1 = "myKey_1";
     public String sheetID = BuildConfig.Sheet_ID;
     String apiKEY = BuildConfig.API_KEY;
-    Button  main_btn2;
+    Button main_btn2;
     public int foreignDict = 0;
-    public int nativeDict = (foreignDict - 1)*(-1);
-    ImageButton btnNewWords,switchLang;
-    int dictSize = 0, val,wordLength = 0,countAppearance,countRightAnswers, col, row, value,value_sum;
-    public String strFWord, strNWord, wordHint, urls,sheetID_1,shtName, langNameFor, langNameNat;
+    public int nativeDict = (foreignDict - 1) * (-1);
+    ImageButton btnNewWords, switchLang;
+    int dictSize = 0, val, wordLength = 0, countAppearance, countRightAnswers, col, row, value, value_sum, countPressForExit = 0;
+    public String strFWord, strNWord, wordHint, urls, sheetID_1, shtName;
     Random rand = new Random();
     JSONArray jsonArray;
-    List<List<String>> listOfListsOfLists =new ArrayList<List<String>>();
+    List<List<String>> listOfListsOfLists = new ArrayList<List<String>>();
     ArrayList<Integer> listMintedProb = new ArrayList<Integer>();
     ArrayList<String> listLangNames = new ArrayList<>();
-    private static TextView twForeignWord,twNativeWord,twTextForeign,twTextNative;
+    private static TextView twForeignWord, twNativeWord, twTextForeign, twTextNative;
     private static EditText editText;
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putInt(STATE_COUNTER, mCounter);
+//    }
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        if (savedInstanceState != null) {
+//            mCounter = savedInstanceState.getInt(STATE_COUNTER, 0);
+//        } else{
+//
+//        }
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         main_btn2 = findViewById(R.id.btn_answer2);
         switchLang = findViewById(R.id.switchLang);
         twForeignWord = findViewById(R.id.foreignWord);
@@ -80,16 +96,17 @@ public class MainActivity extends AppCompatActivity {
         twTextNative = findViewById(R.id.textNative);
         editText = findViewById(R.id.answer);
         btnNewWords = findViewById(R.id.newWordsBtn);
-        sheetID_1 = loadData(this,KEY);
+        sheetID_1 = loadData(this, KEY);
+
 //        sheetID_1 = sheetID;
-        shtName = loadData(this,KEY_1);
+        shtName = loadData(this, KEY_1);
         //Add first run SheetID and SheetName
-        if (sheetID_1.isEmpty()){
+        if (sheetID_1.isEmpty()) {
             Intent intent;
             intent = new Intent(MainActivity.this, AddSheetID.class);
             startActivity(intent);
         }
-        urls ="https://sheets.googleapis.com/v4/spreadsheets/" + sheetID_1 +"/values/"+ shtName +"?key="+apiKEY;
+        urls = "https://sheets.googleapis.com/v4/spreadsheets/" + sheetID_1 + "/values/" + shtName + "?key=" + apiKEY;
         Drawable drawable = editText.getBackground(); // get current EditText drawable
         drawable.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP); // change the drawable color
 
@@ -124,23 +141,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        new GetData().execute();
+
+        if (!haveNetworkConnection()) {
+            Toast.makeText(getApplicationContext(), "Check Internet Connections",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Log.d("my tag", "befour get data");
+            new GetData().execute();
+            Log.d("my tag", "dict_size: " + dictSize);
+        }
+
     }
+
     private void doAnswerAction() {
-        if(editText.getText().toString().trim().equals("")){
-            Toast.makeText(MainActivity.this, "Type your answer", Toast.LENGTH_LONG).show();}
-        else {
+        if (editText.getText().toString().trim().equals("")) {
+            Toast.makeText(MainActivity.this, "Type your answer", Toast.LENGTH_LONG).show();
+        } else {
             boolean checkAnswer = listOfListsOfLists.get(val).get(nativeDict).toUpperCase().trim().equals(editText.getText().toString().toUpperCase().trim());
-            if (checkAnswer & wordLength<=1) {
+            if (checkAnswer & wordLength <= 1) {
                 //Right answer without hints, add 1 to array of right answers
                 Toast.makeText(MainActivity.this, "Wright answer ", Toast.LENGTH_SHORT).show();
-                value = Integer.parseInt(listOfListsOfLists.get(val).get(3))+1;
-                value_sum = Integer.parseInt(listOfListsOfLists.get(val).get(4))+1;
+                value = Integer.parseInt(listOfListsOfLists.get(val).get(3)) + 1;
+                value_sum = Integer.parseInt(listOfListsOfLists.get(val).get(4)) + 1;
                 listOfListsOfLists.get(val).set(3, String.valueOf(value));
                 listOfListsOfLists.get(val).set(4, String.valueOf(value_sum));
-                sendData(val,4,value);
+                sendData(val, 4, value);
                 nextWord(dictSize);
-            } else if (checkAnswer & wordLength>1) {
+            } else if (checkAnswer & wordLength > 1) {
                 //Wright answer, but with hint
                 Toast.makeText(MainActivity.this, "Wright answer ", Toast.LENGTH_SHORT).show();
                 nextWord(dictSize);
@@ -150,34 +177,40 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     public void doHintAction() {
-        if (wordLength < listOfListsOfLists.get(val).get(nativeDict).length()){
-            wordHint = listOfListsOfLists.get(val).get(nativeDict).substring(0, wordLength+1);
+        if (wordLength < listOfListsOfLists.get(val).get(nativeDict).length()) {
+            wordHint = listOfListsOfLists.get(val).get(nativeDict).substring(0, wordLength + 1);
             twNativeWord.setText(wordHint);
             wordLength++;
-        }
-        else{
+        } else {
             Toast.makeText(MainActivity.this, "Whole word is being shown ", Toast.LENGTH_SHORT).show();
         }
     }
-    public static String loadData(Context context,String key) {
+
+    public static String loadData(Context context, String key) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         String text = sharedPreferences.getString(key, "");
         return text;
     }
-    public int getColToSend(){
+
+    public int getColToSend() {
         return this.col;
     }
-    public int getRowToSend(){
+
+    public int getRowToSend() {
         return this.row;
     }
-    public int getValToSend(){
+
+    public int getValToSend() {
         return this.value;
     }
-    public int getInt(int size){
+
+    public int getInt(int size) {
         return rand.nextInt(size);
     }
-    public int calc(int size){
+
+    public int calc(int size) {
         int num = 0;
         if (listMintedProb.isEmpty()) {
             int min = Integer.parseInt(listOfListsOfLists.get(0).get(4));
@@ -186,13 +219,13 @@ public class MainActivity extends AppCompatActivity {
                     min = Integer.parseInt(listOfListsOfLists.get(i).get(4));
                 }
             }
-            for (int i =0; i<size;i++){
+            for (int i = 0; i < size; i++) {
                 if (Integer.parseInt(listOfListsOfLists.get(i).get(4)) == min) {
                     listMintedProb.add(i);
                     num = i;
                 }
             }
-            listMintedProb.remove(listMintedProb.size()-1);
+            listMintedProb.remove(listMintedProb.size() - 1);
         } else {
             int numOfElem = rand.nextInt(listMintedProb.size());
             num = listMintedProb.get(numOfElem);
@@ -200,7 +233,8 @@ public class MainActivity extends AppCompatActivity {
         }
         return num;
     }
-    public void nextWord(int dictSize){
+
+    public void nextWord(int dictSize) {
 //        val = getInt(dictSize);
         val = calc(dictSize);
         twNativeWord.setText("");
@@ -208,14 +242,14 @@ public class MainActivity extends AppCompatActivity {
         wordHint = String.valueOf(0);
         wordLength = 0;
         twForeignWord.setText(listOfListsOfLists.get(val).get(foreignDict));
-        value = Integer.parseInt(listOfListsOfLists.get(val).get(2))+1;
-        value_sum = Integer.parseInt(listOfListsOfLists.get(val).get(4))+1;
-        sendData(val,3,value);
+        value = Integer.parseInt(listOfListsOfLists.get(val).get(2)) + 1;
+        value_sum = Integer.parseInt(listOfListsOfLists.get(val).get(4)) + 1;
+        sendData(val, 3, value);
         listOfListsOfLists.get(val).set(2, String.valueOf(value));
         listOfListsOfLists.get(val).set(4, String.valueOf(value_sum));
     }
-    public class GetData extends AsyncTask<String, Void, String >{
 
+    public class GetData extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... strings) {
             RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
@@ -224,27 +258,26 @@ public class MainActivity extends AppCompatActivity {
                 public void onResponse(JSONObject response) {
                     jsonArray = response.optJSONArray("values");
                     int len = jsonArray.length();
-                    for (int i = 0;i < len; i++){
+                    for (int i = 0; i < len; i++) {
                         List<String> listOfStrings = new ArrayList<String>();
                         try {
                             JSONArray json = jsonArray.optJSONArray(i);
                             strFWord = json.getString(0);
                             strNWord = json.getString(1);
-                            if (json.length()==2){
+                            if (json.length() == 2) {
                                 json.put("0");
                                 json.put("0");
-                            } else if (json.length()==3){
+                            } else if (json.length() == 3) {
                                 json.put("0");
                             }
-
                             try {
                                 countAppearance = json.getInt(2);
-                            } catch (Exception e){
+                            } catch (Exception e) {
                                 countAppearance = 0;
                             }
                             try {
                                 countRightAnswers = json.getInt(3);
-                            }catch (Exception e){
+                            } catch (Exception e) {
                                 countRightAnswers = 0;
                             }
                             int value = countAppearance + countRightAnswers;
@@ -268,7 +301,6 @@ public class MainActivity extends AppCompatActivity {
                     twTextForeign.setText(listLangNames.get(foreignDict));
                     twTextNative.setText(listLangNames.get(nativeDict));
                     nextWord(dictSize);
-
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -279,30 +311,33 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
-    private void sendData(int row, int col, int value_new){
-        this.row = row+2;
+
+    private void sendData(int row, int col, int value_new) {
+        this.row = row + 2;
         this.col = col;
         this.value = value_new;
 //        new SendData().execute();
         new SendMyData().execute();
     }
+
     public class SendMyData extends AsyncTask<String, Void, String> {
         int col = MainActivity.this.col;
         int row = MainActivity.this.row;
         int value = MainActivity.this.value;
+
         @Override
         protected String doInBackground(String... strings) {
-            try{
+            try {
                 URL url = new URL("https://script.google.com/macros/s/AKfycbyn2hquKrCHYtTv8d-CTimqbnZcr1ZoWzppd9QnORbiHd4zJkoINjmHRSksbg1YgJDNBg/exec");
 //                        "https://script.google.com/macros/s/AKfycbwFAIvRwhMXr3VkLtsWgnJpODv7oQD5kruE1RSABnNrpi1H1qm6QZ-5qj6SE1F5ozzj7w/exec");
                 //
                 JSONObject postDataParams = new JSONObject();
 
-                postDataParams.put("values",value);
-                postDataParams.put("shtID",sheetID_1);
-                postDataParams.put("shtName",shtName);
-                postDataParams.put("rowNum",row);
-                postDataParams.put("colNum",col);
+                postDataParams.put("values", value);
+                postDataParams.put("shtID", sheetID_1);
+                postDataParams.put("shtName", shtName);
+                postDataParams.put("rowNum", row);
+                postDataParams.put("colNum", col);
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setReadTimeout(15000 /* milliseconds */);
                 conn.setConnectTimeout(15000 /* milliseconds */);
@@ -319,15 +354,15 @@ public class MainActivity extends AppCompatActivity {
                 writer.close();
                 os.close();
 
-                int responseCode=conn.getResponseCode();
+                int responseCode = conn.getResponseCode();
 
                 if (responseCode == HttpsURLConnection.HTTP_OK) {
 
-                    BufferedReader in=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                     StringBuffer sb = new StringBuffer("");
-                    String line="";
+                    String line = "";
 
-                    while((line = in.readLine()) != null) {
+                    while ((line = in.readLine()) != null) {
                         sb.append(line);
                         break;
                     }
@@ -335,28 +370,28 @@ public class MainActivity extends AppCompatActivity {
                     in.close();
                     return sb.toString();
 
+                } else {
+                    return new String("false : " + responseCode);
                 }
-                else {
-                    return new String("false : "+responseCode);
-                }
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 return new String("Exception: " + e.getMessage());
             }
         }
+
         protected void onPostExecute(String result) {
 //        Toast.makeText(getApplicationContext(), "Word added",
 //                Toast.LENGTH_SHORT).show();
         }
+
         public String getPostDataString(JSONObject params) throws Exception {
 
             StringBuilder result = new StringBuilder();
             boolean first = true;
 
             Iterator<String> itr = params.keys();
-            while(itr.hasNext()){
+            while (itr.hasNext()) {
 
-                String key= itr.next();
+                String key = itr.next();
                 Object value = params.get(key);
 
                 if (first)
@@ -374,11 +409,36 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-    public void switchLan(){
-        foreignDict = (foreignDict -1)*(-1);
-        nativeDict = (foreignDict - 1)*(-1);
+
+    public void switchLan() {
+        foreignDict = (foreignDict - 1) * (-1);
+        nativeDict = (foreignDict - 1) * (-1);
         twTextForeign.setText(listLangNames.get(foreignDict));
         twTextNative.setText(listLangNames.get(nativeDict));
         nextWord(dictSize);
     }
+
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
+        finish();
+    }
+
 }
