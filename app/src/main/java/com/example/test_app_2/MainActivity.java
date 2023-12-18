69c47625ded090e985e32befc2652826c28b8d7f
Sergey <akhmadulinsergey@gmail.com>
2023-12-18 09:53:13 +0200

    s

--- app/src/main/java/com/example/test_app_2/MainActivity.java
+++ app/src/main/java/com/example/test_app_2/MainActivity.java
@@ -44,11 +44,18 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Iterator;
@@ -658,6 +665,5 @@ public class MainActivity extends AppCompatActivity {
         finishAffinity();
         finish();
     }
-
 
 }
